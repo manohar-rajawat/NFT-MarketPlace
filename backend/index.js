a6f23c375036f06fd64d42da28bed643565bc184
Manohar Singh Rajawat <manoharsinghrajawat@SDFSs-MacBook-Pro-2.local>
2024-02-08 17:24:34 -0800

    Feat: adding getcontractnft api

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,24 @@ app.get("/getnftdata", async (req, res) => {
     }
 });
 
+app.get("/getcontractnft", async (req, res) => {
+    try {
+        const { query } = req;
+        const chain = query.chain == "0x5" ? "0x5" : "0x1";
+
+        const response = await Moralis.EvmApi.nft.getContractNFTs({
+            chain,
+            format: "decimal",
+            address: query.contractAddress,
+        });
+
+        return res.status(200).json(response);
+    } catch (e) {
+        console.log(`Something went wrong ${e}`);
+        return res.status(400).json();
+    }
+});
+
 Moralis.start({
     apiKey: MORALIS_API_KEY,
 }).then(() => {
